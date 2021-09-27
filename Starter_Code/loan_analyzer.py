02786a00958884e64bb3f7a354b31c7dbd491540
anythingelse-sf <89985811+anythingelse-sf@users.noreply.github.com>
2021-09-26 19:50:50 -0700

    Part 3 complete
    Run a function to return present value

--- Starter_Code/loan_analyzer.py
+++ Starter_Code/loan_analyzer.py
@@ -2,94 +2,94 @@
 import csv
 from pathlib import Path
 
-"""Part 1: Automate the Calculations.
+# """Part 1: Automate the Calculations.
 
- Automate the calculations for the loan portfolio summaries.
+#  Automate the calculations for the loan portfolio summaries.
 
-First, let's start with some calculations on a list of prices for 5 loans.
-    1. Use the `len` function to calculate the total number of loans in the list.
-    2. Use the `sum` function to calculate the total of all loans in the list.
-    3. Using the sum of all loans and the total number of loans, calculate the average loan price.
-    4. Print all calculations with descriptive messages.
-"""
-loan_costs = [500, 600, 200, 1000, 450]
+# First, let's start with some calculations on a list of prices for 5 loans.
+#     1. Use the `len` function to calculate the total number of loans in the list.
+#     2. Use the `sum` function to calculate the total of all loans in the list.
+#     3. Using the sum of all loans and the total number of loans, calculate the average loan price.
+#     4. Print all calculations with descriptive messages.
+# """
+# loan_costs = [500, 600, 200, 1000, 450]
 
-# How many loans are in the list?
-how_many_loans = len(loan_costs)
-# Print the number of loans from the list
-print("The number of loans from this list:", how_many_loans)
+# # How many loans are in the list?
+# how_many_loans = len(loan_costs)
+# # Print the number of loans from the list
+# print("The number of loans from this list:", how_many_loans)
 
-# What is the total of all loans?
-total_of_all_loans = sum(loan_costs)
-# Print the total value of the loans
-print("The total value of the loans:", total_of_all_loans)
+# # What is the total of all loans?
+# total_of_all_loans = sum(loan_costs)
+# # Print the total value of the loans
+# print("The total value of the loans:", total_of_all_loans)
 
-# What is the average loan amount from the list?
-average_loan_amount = total_of_all_loans / how_many_loans
-# Print the average loan amount
-print("The average loan amount is:", average_loan_amount)
+# # What is the average loan amount from the list?
+# average_loan_amount = total_of_all_loans / how_many_loans
+# # Print the average loan amount
+# print("The average loan amount is:", average_loan_amount)
 
-"""Part 2: Analyze Loan Data.
+# """Part 2: Analyze Loan Data.
 
-Analyze the loan to determine the investment evaluation.
+# Analyze the loan to determine the investment evaluation.
 
-Using more detailed data on one of these loans, follow these steps to calculate a Present Value, or a "fair price" for what this loan would be worth.
+# Using more detailed data on one of these loans, follow these steps to calculate a Present Value, or a "fair price" for what this loan would be worth.
 
-1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
-    a. Save these values as variables called `future_value` and `remaining_months`.
-    b. Print each variable.
+# 1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
+#     a. Save these values as variables called `future_value` and `remaining_months`.
+#     b. Print each variable.
 
-    @NOTE:
-    **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
-    **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
+#     @NOTE:
+#     **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
+#     **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
 
-2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum required return of 20% as the discount rate.
-3. Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-    a. If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-    b. Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
+# 2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum required return of 20% as the discount rate.
+# 3. Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
+#     a. If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
+#     b. Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 
-    @NOTE:
-    If Present Value represents the loan's fair value (given the required minimum return of 20%), does it make sense to buy the loan at its current cost?
-"""
+#     @NOTE:
+#     If Present Value represents the loan's fair value (given the required minimum return of 20%), does it make sense to buy the loan at its current cost?
+# """
 
-# Given the following loan data, you will need to calculate the present value for the loan
-loan = {
-    "loan_price": 500,
-    "remaining_months": 9,
-    "repayment_interval": "bullet",
-    "future_value": 1000,
-}
+# # Given the following loan data, you will need to calculate the present value for the loan
+# loan = {
+#     "loan_price": 500,
+#     "remaining_months": 9,
+#     "repayment_interval": "bullet",
+#     "future_value": 1000,
+# }
 
-# @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
-# Print each variable.
+# # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
+# # Print each variable.
 
-future_value = loan.get("future_value")
-print("The Future Value is:", future_value)
+# future_value = loan.get("future_value")
+# print("The Future Value is:", future_value)
 
-remaining_months = loan.get("remaining_months")
-print("There are", remaining_months, "months remaining")
+# remaining_months = loan.get("remaining_months")
+# print("There are", remaining_months, "months remaining")
 
 
-# @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
-# Use a minimum required return of 20% as the discount rate.
-#   You'll want to use the **monthly** version of the present value formula.
-#   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
+# # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
+# # Use a minimum required return of 20% as the discount rate.
+# #   You'll want to use the **monthly** version of the present value formula.
+# #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 
-present_value = future_value / (1+.2/12) ** remaining_months
-print(f"The Present Value is {present_value: .2f}")
+# present_value = future_value / (1+.2/12) ** remaining_months
+# print(f"The Present Value is {present_value: .2f}")
 
-# If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
-# @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-#    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-#    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
+# # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
+# # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
+# #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
+# #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 
-loan_price =loan.get("loan_price")
-print("The loan price is:", loan_price)
+# loan_price =loan.get("loan_price")
+# print("The loan price is:", loan_price)
 
-if (present_value) >= (loan_price):
-    print("The loan is worth at least the cost to buy it")
-else:
-    print("The loan is too expensive and not worth the price")
+# if (present_value) >= (loan_price):
+#     print("The loan is worth at least the cost to buy it")
+# else:
+#     print("The loan is too expensive and not worth the price")
 
 
 """Part 3: Perform Financial Calculations.
@@ -114,13 +114,19 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
+
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    present_value = future_value / (1+annual_discount_rate/remaining_months) ** 12
+    return present_value
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
-# YOUR CODE HERE!
-#print(f"The present value of the loan is: {present_value}")
+annual_discount_rate = .2
+new_loan = calculate_present_value(future_value=1000, remaining_months=12, annual_discount_rate=.2)
+
+# calculate_present_value(new_loan)
+print(f"The present value of the loan is: {new_loan: .2f}")
 
 
 """Part 4: Conditionally filter lists of loans.
